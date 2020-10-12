635c3d86b9a7b4070adbeffa7a1b1f8a46392659
Hakeem Almidan <hakeemmidan@gmail.com>
2020-10-12 21:49:28 +0300

    Update resume

--- main.js
+++ main.js
@@ -3,56 +3,64 @@ let workExperience = new Vue({
   data: {
     positions: [
       {
-        title: "Full-Stack Software Engineer",
-        company: "Fiskkit",
-        timeFrame: "Dec 2019 - present",
+        title:
+          'Graduate Development Program - Technology (Cybersecurity/IT) Track',
+        company: 'Public Investment Fund of Saudi Arabia',
+        timeFrame: 'Aug 2020 - present',
+        tasks: [],
+      },
+      {
+        title: 'Full-Stack Software Engineer',
+        company: 'Fiskkit',
+        timeFrame: 'Dec 2019 - present',
         tasks: [
-          "Worked in Agile environment with a team of 5 developers and 1 designer to optimize development, testing and deployment",
-          "Implemented email confirmation full-stack feature through Mailchimp's Mandrill API allowing for authenticated signups",
-          "Integrated and customized Google Analytics into React / Redux to track many metrics including traffic sources, session duration, and general user interaction; allowing team to pinpoint areas that need improvement",
-          'Led screen-responsiveness feature of entire website by utilizing CSS media queries and React, making it mobile/ tablet responsive',
-          "Made 40 + (ongoing) frontend and backend enhancements including mission critical bug fixes that caused entire page errors",
-          "Leveraged knowledge in (hard) React, Redux, Sass, Laravel, MySQL, Docker, (soft) teamwork, and problem solving"
-        ]
+          'Worked in Agile environment with a team of 5 developers and 1 designer to optimize development, testing and deployment. This required continuous communication and work overlap',
+          "Implemented email confirmation full-stack feature through Mailchimp's Mandrill API, allowing for authenticated emails/signups",
+          'Integrated and customized Google Analytics into React/Redux to track many metrics including traffic sources, session duration, and general user interaction; allowing team to pinpoint areas that need improvement',
+          'Led screen-responsiveness of entire website by utilizing CSS media queries and React, increasing mobile users proportion by +23%',
+          'Made 40+ (ongoing) front-end and back-end enhancements including mission critical bug fixes that caused entire page errors',
+          'Leveraged knowledge in (hard) React, Redux, Sass, Laravel, MySQL, Docker, (soft) teamwork, and problem solving',
+        ],
       },
       {
         title: 'Full-Stack Software Engineering Student',
         company: 'App Academy',
         timeFrame: 'Jun 2019 - Dec 2019',
         tasks: [
-          "Participated in daily pair-programming sessions involving many technical challenges ranging from database abstraction layer, data access layer, to the presentation layer.This required continuous learning, communication, and switching in coding environments",
-          "Completed 17 intensive 90-100 hour work weeks involving lectures, weekly meetings, daily standup meetings, pair-programming, individual programming, technical reading, and presentation, resulting in significant increase in programming abilities",
-          "Leveraged knowledge in (hard) SQL, MongoDB, Ruby on Rails, Express, React, Redux, Sass, CSS, RSpec, Capybara(soft) communication, teamwork, adaptability, problem solving, time management, leadership, and attention to detail "
-        ]
+          'Participated in daily pair-programming sessions involving many technical challenges ranging from database abstraction layer, data access layer, to the presentation layer. This required continuous learning, communication, and switching in coding environments',
+          'Completed 17 intensive 90-100 hour work weeks involving lectures, weekly meetings, daily standup meetings, pair-programming, individual programming, technical reading, and presentation, resulting in significant increase in programming abilities',
+          'Leveraged knowledge in (hard) SQL, MongoDB, Ruby on Rails, Express, React, Redux, Sass, CSS, RSpec, Capybara (soft) communication, teamwork, adaptability, problem solving, time management, leadership, and attention to detail ',
+        ],
       },
       {
-        title: "Student Advisory Board Member",
-        company: "Student Leadership & Involvement (SLI) at Oregon State University",
-        timeFrame: "Sep 2018 – Dec 2018",
+        title: 'Student Advisory Board Member',
+        company:
+          'Student Leadership & Involvement (SLI) at Oregon State University',
+        timeFrame: 'Sep 2018 – Dec 2018',
         tasks: [
           'Reviewed and made recommendations regarding proposed increases in the annual budget of the Student Leadership & Involvement',
           'Recommended the student fee amount required to support the Student Leadership & Involvement annual budget request to the Budget Manager',
           'Reviewed unit programs, services, and facilities and made recommendations to staff to support and enhance the success of OSU students',
           'Served as a sounding board for the leadership of the SLI unit',
           'Served as a liaison between the ASOSU, SFC, and SLI unit',
-          'Leveraged knowledge in (hard) MS Excel, VBA, (soft) communication, public speaking, and networking'
-        ]
+          'Leveraged knowledge in (hard) MS Excel, VBA, (soft) communication, public speaking, and networking',
+        ],
       },
       {
-        title: "Communication Representative",
-        company: "Ettihad Cultural Center (ECC) at Oregon State University",
-        timeFrame: "Aug 2017 - Jun 2018",
+        title: 'Communication Representative',
+        company: 'Ettihad Cultural Center (ECC) at Oregon State University',
+        timeFrame: 'Aug 2017 - Jun 2018',
         tasks: [
-          "Organized 10 + events, including the Ettihad Cultural Festival, which hosted 2000 + walking attendees.This required communication with hundreds of contacts, event planning, weekly meetings, and lots of teamwork",
-          "Directed team of 15 people through rehearsals and performance in Ettihad Cultural Festival, resulting in error-free performance",
-          "Acted as MC for 2 events that collaborated with other university departments, enhancing my public speaking and leadership",
-          "Started programming in Ruby and JavaScript to solve daily abstract problems involving the cultural center",
-          "Leveraged knowledge in(hard) Ruby, JavaScript, VBA(soft) communication, public speaking, event planning, teamwork, and leadership "
-        ]
+          'Organized 10+ events, including the Ettihad Cultural Festival, which hosted 2000+ walking attendees. This required communication with hundreds of contacts, event planning, weekly meetings, and lots of teamwork',
+          'Directed team of 15 people through rehearsals and performance in Ettihad Cultural Festival, resulting in error-free performance',
+          'Acted as MC for 2 events that collaborated with other university departments, increasing sign ups by 30+ new student emails',
+          'Started programming in Ruby and JavaScript to solve daily abstract problems involving the cultural center',
+          'Leveraged knowledge in (hard) Ruby, JavaScript, VBA (soft) communication, public speaking, event planning, teamwork, and leadership ',
+        ],
       },
-    ]
-  }
-})
+    ],
+  },
+});
 
 let education = new Vue({
   el: "#education",
@@ -90,7 +98,7 @@ let projects = new Vue({
         description: 'A polling app where users submit two pictures for their friends to vote on',
         highlights: [
           'Collaborated with 2 other full-stack developers and team lead in an Agile environment using ZenHub for ticket management',
-          'Wrote integration tests(35 specs) using Mocha, Chai and in-memory mock-database for better app maintainability',
+          'Wrote integration tests (35 specs) using Mocha, Chai and in-memory mock-database for better app maintainability',
           'Led and maintained cookie-based authentication API routes using http-cookies and json web tokens for better security',
           'Created friends React context that dynamically updates friends as they are followed / unfollowed for better UX',
           'Led project presentations during evaluations'
@@ -178,9 +186,9 @@ let projects = new Vue({
         title: 'Jewely',
         description: 'An Etsy inspired [mock] e-commerce marketplace',
         highlights: [
-          'Connected window.localStorage with shopping cart to avoid querying database every time a user updates cart; resulting in faster site performance',
-          'Implemented sign-in form autofill by utilizing jQuery for element selection and JavaScript asynchronous behavior for animated appearance',
-          'Integrated multi-photo attachment feature using Amazon S3, JSON JBuilder, and Ruby on Rails’ Active Storage'
+          'Learned and merged Redux with React and created easily accessible global state for faster site performance, and easier frontend development process',
+          'Utilized Active Record and Action Controller for fetching and processing data through RESTful APIs, standardizing APIs',
+          'Integrated multi-photo attachment feature using Amazon S3, JSON JBuilder, and Ruby on Rails’ Active Storage for better UI/UX'
         ],
         tech: {
           database: 'PostgreSQL',
