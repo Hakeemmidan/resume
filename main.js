71d81efd659d40e055534105d186c0103f82fd51
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2021-08-22 18:38:34 +0300

    update content

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let workExperience = new Vue({
         timeFrame: 'Mar 2021 - present',
         tasks: [
           'Interviewed software engineering candidates',
-          'Made 43 [merged] Pull Requests on GitHub [so far], that included various changes from new features to error fixes',
+          'Made 49 merged Pull Requests on GitHub, that included various changes from new features to error fixes',
           'Leveraging knowledge in (hard) React, Shopify’s Polaris, styled-components, Ruby on Rails, RSpec, Active Record, Action Controller, Active Support, Active Job, (soft) agile software development, teamwork, and problem-solving',
         ],
         employment: '(full-time)'
